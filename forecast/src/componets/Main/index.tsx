c57d4d18dd1385f3096516f0673efdff10fa46bd
kky04994 <kkyo8747@naver.com>
2022-05-26 22:34:12 +0900

    add main components file

---
+++ forecast/src/componets/Main/index.tsx
@@ -0,0 +1,14 @@
+import Company from "./Company";
+import Search from "./Search";
+import * as S from "./styles";
+
+const Main = () => {
+  return (
+    <S.Container>
+      <Company />
+      <Search />
+    </S.Container>
+  );
+};
+
+export default Main;
---
+++ forecast/src/componets/Main/styles.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  width: 88%;
+  height: 806px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: space-between;
+`;
